87405aad5831344dc4fde51c25c089c9078f9b96
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2022-12-16 15:28:56 -0300

    init

---
+++ components/variables/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styled';
+import React from 'react';
+
+const Equation: React.FunctionComponent = (): JSX.Element => {
+  return (
+    <S.Wrapper>
+      <p>Equation</p>
+    </S.Wrapper>
+  );
+};
+
+export default Equation;
