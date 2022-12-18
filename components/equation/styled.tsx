7255ab35fca19f4eb87399253ba57c362b7e813e
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2022-12-17 21:08:18 -0300

    style input equation

--- components/equation/styled.tsx
+++ components/equation/styled.tsx
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  width: 100%;
+  max-width: 40rem;
+`;
 
 export const EquationInput = styled.textarea`
   height: 10rem;
   width: 100%;
+  padding: 1rem;
   background-color: black;
   font-family: 'Courier New', Courier, monospace;
   font-size: large;
   color: #f2f2f3;
   resize: none;
+  border: none;
+  outline: none;
 `;
 
 export const Preview = styled.div`
