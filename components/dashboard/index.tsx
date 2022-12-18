3d6b0e5960ba3cc101c7d5ac662a15efdf06e7e9
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2022-12-18 01:11:19 -0300

    math avaluate and simplify

--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -1,11 +1,68 @@
 import * as S from './styled';
-import React, { FunctionComponent, useContext } from 'react';
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
+import { simplify, evaluate } from 'mathjs';
 import { context } from '../../pages/equation';
 
+type Expression = {
+  simplify: {
+    value: string;
+    error: string | null;
+  };
+  evaluate: {
+    value: string;
+    error: string | null;
+  };
+};
+
+const updateExpession = (
+  equation: string,
+  method: (equation: string) => any
+) => {
+  try {
+    return {
+      value: simplify(equation as string).toString(),
+      error: null,
+    };
+  } catch (error) {
+    let message = 'erro desconhecido';
+    if (error instanceof Error) message = error.message;
+    console.warn(message);
+    return {
+      value: 'error',
+      error: message,
+    };
+  }
+};
+
 const Dashboard: FunctionComponent = (): JSX.Element => {
+  const [expression, setExpression] = useState<Expression>({
+    simplify: { value: '', error: null },
+    evaluate: { value: '', error: null },
+  });
   const formula = useContext(context);
-  return <S.Wrapper><S.Simplify></S.Simplify></S.Wrapper>;
+  const equation = formula?.equation;
+
+  useEffect(() => {
+    if (expression && equation) {
+      setExpression({
+        simplify: updateExpession(equation, simplify),
+        evaluate: updateExpession(equation, evaluate),
+      });
+    }
+  }, [equation]);
+
+  return (
+    <S.Wrapper>
+      <S.Simplify>{expression?.simplify?.value}</S.Simplify>
+      <S.Simplify>{expression?.evaluate?.value}</S.Simplify>
+    </S.Wrapper>
+  );
 };
 
 export default Dashboard;
--- components/dashboard/styled.tsx
+++ components/dashboard/styled.tsx
@@ -8,6 +8,9 @@ export const Simplify = styled.div`
   background-color: #0a0a0c;
   border-radius: 5px;
   margin: 1rem;
+  padding: 1.5rem;
   height: 10rem;
   width: 50%;
+  font-size: larger;
+  color: #f1f1f2;
 `
